bba9a5738ffdb11c0a5fd3a09784bb3b74971b48
Lais <laisaplemos10@gmail.com>
2025-11-26 22:39:37 -0200

    arrumando

--- src/Pedido.java
+++ src/Pedido.java
@@ -1,5 +1,4 @@
 import java.io.Serializable;
-import java.util.concurrent.*;
 
 public class Pedido extends Comunicado implements Serializable {
     private byte[] numeros;
@@ -12,65 +11,45 @@ public class Pedido extends Comunicado implements Serializable {
         return numeros;
     }
 
-    // -------------------------------
-    // MÉTODO ORDENAR - Merge Sort Paralelo
-    // -------------------------------
+    // ---------------------------------------------------
+    // Método ordenar(): Merge Sort sequencial
+    // ---------------------------------------------------
     public void ordenar() {
         if (numeros == null || numeros.length <= 1)
             return;
 
-        int maxThreads = Runtime.getRuntime().availableProcessors();
-        ForkJoinPool pool = new ForkJoinPool(maxThreads);
-        pool.invoke(new MergeSortTask(numeros, 0, numeros.length - 1));
-        pool.shutdown();
+        mergeSort(0, numeros.length - 1);
     }
 
-    // -------------------------------
-    // TAREFA RECURSIVA PARA MERGE SORT
-    // -------------------------------
-    private static class MergeSortTask extends RecursiveAction {
-        private final byte[] vetor;
-        private final int inicio, fim;
+    private void mergeSort(int inicio, int fim) {
+        if (inicio >= fim) return;
 
-        MergeSortTask(byte[] vetor, int inicio, int fim) {
-            this.vetor = vetor;
-            this.inicio = inicio;
-            this.fim = fim;
+        int meio = (inicio + fim) / 2;
+
+        mergeSort(inicio, meio);
+        mergeSort(meio + 1, fim);
+
+        merge(inicio, meio, fim);
+    }
+
+    private void merge(int ini, int meio, int fim) {
+        int tam1 = meio - ini + 1;
+        int tam2 = fim - meio;
+
+        byte[] esq = new byte[tam1];
+        byte[] dir = new byte[tam2];
+
+        for (int i = 0; i < tam1; i++) esq[i] = numeros[ini + i];
+        for (int j = 0; j < tam2; j++) dir[j] = numeros[meio + 1 + j];
+
+        int i = 0, j = 0, k = ini;
+
+        while (i < tam1 && j < tam2) {
+            if (esq[i] <= dir[j]) numeros[k++] = esq[i++];
+            else numeros[k++] = dir[j++];
         }
 
-        @Override
-        protected void compute() {
-            if (inicio >= fim) return;
-
-            int meio = (inicio + fim) / 2;
-
-            MergeSortTask esquerda = new MergeSortTask(vetor, inicio, meio);
-            MergeSortTask direita = new MergeSortTask(vetor, meio + 1, fim);
-
-            invokeAll(esquerda, direita); // executa as duas em paralelo
-
-            merge(vetor, inicio, meio, fim); // junta os resultados
-        }
-
-        private void merge(byte[] v, int ini, int meio, int fim) {
-            int tam1 = meio - ini + 1;
-            int tam2 = fim - meio;
-
-            byte[] esq = new byte[tam1];
-            byte[] dir = new byte[tam2];
-
-            for (int i = 0; i < tam1; i++) esq[i] = v[ini + i];
-            for (int j = 0; j < tam2; j++) dir[j] = v[meio + 1 + j];
-
-            int i = 0, j = 0, k = ini;
-
-            while (i < tam1 && j < tam2) {
-                if (esq[i] <= dir[j]) v[k++] = esq[i++];
-                else v[k++] = dir[j++];
-            }
-
-            while (i < tam1) v[k++] = esq[i++];
-            while (j < tam2) v[k++] = dir[j++];
-        }
+        while (i < tam1) numeros[k++] = esq[i++];
+        while (j < tam2) numeros[k++] = dir[j++];
     }
 }
